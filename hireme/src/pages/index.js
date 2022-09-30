e789c6d3ee4927fc22de382458a4ef1dc76483a7
Sandra <sgury@student.42.fr>
2022-09-30 10:31:18 -0700

    clean architecture

--- hireme/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Home } from './Home';
-export { default as Green } from './Green';
-export { default as Hireme } from './Hireme';
-export { default as Resume } from './Resume';
-export { default as Projects } from './Projects';
-export { default as Resources } from './Resources';
-export { default as PageNotFound } from './PageNotFound';
